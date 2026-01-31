6f41321142a179438a5ac27d2671c1a208ce7baf
Raghad <raghadmahgoub0303@gmail.com>
2026-01-31 15:38:40 +0200

    edaagent for raw data

--- src/eda/runner.py
+++
@@ -1,9 +0,0 @@
-from .dataset_summary import inspect_dataset
-
-def run_eda(df, target_col=None):
-    """
-    Public entry point for EDA.
-    """
-    return {
-        "dataset_summary": inspect_dataset(df, target_col)
-    }
--- src/eda/test.py
+++
@@ -1,9 +0,0 @@
-from src.eda.runner import run_eda
-import pandas as pd
-
-df = pd.DataFrame({
-    "a": [1, 2, 3],
-    "b": ["x", "y", "z"]
-})
-
-print(run_eda(df, target_col="a"))
